b8bc1d71e29669cf55d1f77bcc581952ea9b9920
Tammibriggs <Godsfirstbriggs@gmail.com>
2021-11-25 08:34:44 +0100

    created functionality for adding new task

--- src/AddTask.js
+++ src/AddTask.js
@@ -1,16 +1,33 @@
 import Modal from "./Modal"
 import {useState} from 'react'
 import './addTask.css'
-
+import {db} from './firebase'
+import {collection, addDoc} from 'firebase/firestore'
+import {useStateValue} from './StateProvider.js'
 
 function AddTask({onClose, open}) {
+
+  const {setOpen} = useStateValue()
 
   const [task, setTask] = useState('')
   const [description, setDescription] = useState('')
 
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      await addDoc(collection(db, 'tasks'), {
+        task: task,
+        description: description
+      })
+        setOpen({...open, 'add': false})
+    } catch (err) {
+      console.log('failed to respond', err)
+    }
+  }
+
   return (
     <Modal modalLable='Add Task' onClose={onClose} open={open}>
-      <form className='addTask'>
+      <form onSubmit={handleSubmit} className='addTask'>
         <input 
           type='text' 
           name='task' 
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD5Kj7NU256TKVncdOCy_TELSmdRrEAH4c",
+  authDomain: "task-manager-1e712.firebaseapp.com",
+  projectId: "task-manager-1e712",
+  storageBucket: "task-manager-1e712.appspot.com",
+  messagingSenderId: "935257286219",
+  appId: "1:935257286219:web:d88cfe3837e8e9a4e2cfce"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+
+export {db}
